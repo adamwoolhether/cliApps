736d14f1824f646ff06706bf6747ecbff0a54f20
adamwoolhether <adamwoolhether@gmail.com>
2022-01-21 13:16:01 +0800

    added bytes counter

--- 01/wc/main.go
+++ 01/wc/main.go
@@ -11,20 +11,24 @@ import (
 func main() {
 	// Define a boolean flag -l to count lines instead of words.
 	lines := flag.Bool("l", false, "Count lines")
+	bytes := flag.Bool("b", false, "Count bytes")
 	// Parse the given flags.
 	flag.Parse()
 
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
 // count returns the number of words given by io.Reader.
-func count(r io.Reader, countLines bool) int {
-	// A scanner reads text from a reader.
+func count(r io.Reader, countLines, countBytes bool) int {
+	// A scanner reads text from the reader.
 	scanner := bufio.NewScanner(r)
 
 	// If count lines flag isn't set, we count the words.
 	if !countLines {
 		scanner.Split(bufio.ScanWords)
+	}
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
 	}
 
 	// Define a counter.
