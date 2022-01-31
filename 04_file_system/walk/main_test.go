c5aed11edbeacbbe0272dd044f2b4bee8cd84171
adamwoolhether <adamwoolhether@gmail.com>
2022-01-31 12:39:50 +0800

    Logging for deleted files

--- 04_file_system/walk/main_test.go
+++ 04_file_system/walk/main_test.go
@@ -58,7 +58,12 @@ func TestRunDelExtension(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			var buffer bytes.Buffer
+			var (
+				buffer    bytes.Buffer
+				logBuffer bytes.Buffer
+			)
+			tc.cfg.wLog = &logBuffer
+
 			tempDir, cleanup := createTempDir(t, map[string]int{
 				tc.cfg.ext:     tc.nDelete,
 				tc.extNoDelete: tc.nNoDelete,
@@ -80,6 +85,12 @@ func TestRunDelExtension(t *testing.T) {
 			}
 			if len(filesLeft) != tc.nNoDelete {
 				t.Errorf("Expected %d files left, got %d\n", tc.nNoDelete, len(filesLeft))
+			}
+
+			expLogLines := tc.nDelete + 1
+			lines := bytes.Split(logBuffer.Bytes(), []byte("\n"))
+			if len(lines) != expLogLines {
+				t.Errorf("Expected %d log lines, got %d\n", expLogLines, len(lines))
 			}
 		})
 	}
