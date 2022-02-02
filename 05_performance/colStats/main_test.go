86233708b7d43fd690176261272b565cb9365b84
adamwoolhether <adamwoolhether@gmail.com>
2022-02-02 11:36:49 +0800

    Benchmark - initial benchmark implemented

--- 05_performance/colStats/main_test.go
+++ 05_performance/colStats/main_test.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bytes"
 	"errors"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -44,5 +46,22 @@ func TestRun(t *testing.T) {
 				t.Errorf("expected %q, got %q", tc.exp, &res)
 			}
 		})
+	}
+}
+
+// gotest -bench . -run ^$
+// gotest -bench . -benchtime=10x -run ^$
+func BenchmarkRun(b *testing.B) {
+	filenames, err := filepath.Glob("./testdata/benchmark/*.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		if err = run(filenames, "avg", 2, ioutil.Discard); err != nil {
+			b.Error(err)
+		}
 	}
 }
