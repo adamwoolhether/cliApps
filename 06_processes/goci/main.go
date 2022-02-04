81ffcf614dd1e5ff17d63397877aa0b26d6bbba8
adamwoolhether <adamwoolhether@gmail.com>
2022-02-04 16:19:14 +0800

    TimeoutStep, 4th pipeline added

--- 06_processes/goci/main.go
+++ 06_processes/goci/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 type executor interface {
@@ -26,10 +27,11 @@ func run(proj string, out io.Writer) error {
 		return fmt.Errorf("project directory is required: %w", ErrValidation)
 	}
 
-	pipeline := make([]executor, 3)
+	pipeline := make([]executor, 4)
 	pipeline[0] = newStep("go build", "go", "Go Build: SUCCESS", proj, []string{"build", ".", "errors"})
 	pipeline[1] = newStep("go test", "go", "Go Test: SUCCESS", proj, []string{"test", "-v"})
 	pipeline[2] = newExceptionStep("go fmt", "gofmt", "Gofmt: SUCCESS", proj, []string{"-l", "."})
+	pipeline[3] = newTimeoutStep("git push", "git", "Git Push: SUCCESS", proj, []string{"push", "origin", "master"}, 10*time.Second)
 
 	for _, s := range pipeline {
 		msg, err := s.execute()
