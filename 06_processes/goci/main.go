ef9357ac337a1590b26f8f3f6b341ac37510e63a
adamwoolhether <adamwoolhether@gmail.com>
2022-02-04 17:03:14 +0800

    Integration test with local temp git server

--- 06_processes/goci/main.go
+++ 06_processes/goci/main.go
@@ -28,10 +28,35 @@ func run(proj string, out io.Writer) error {
 	}
 
 	pipeline := make([]executor, 4)
-	pipeline[0] = newStep("go build", "go", "Go Build: SUCCESS", proj, []string{"build", ".", "errors"})
-	pipeline[1] = newStep("go test", "go", "Go Test: SUCCESS", proj, []string{"test", "-v"})
-	pipeline[2] = newExceptionStep("go fmt", "gofmt", "Gofmt: SUCCESS", proj, []string{"-l", "."})
-	pipeline[3] = newTimeoutStep("git push", "git", "Git Push: SUCCESS", proj, []string{"push", "origin", "master"}, 10*time.Second)
+
+	pipeline[0] = newStep(
+		"go build",
+		"go",
+		"Go Build: SUCCESS",
+		proj, []string{"build", ".", "errors"},
+	)
+
+	pipeline[1] = newStep(
+		"go test",
+		"go",
+		"Go Test: SUCCESS",
+		proj, []string{"test", "-v"},
+	)
+
+	pipeline[2] = newExceptionStep(
+		"go fmt",
+		"gofmt",
+		"Gofmt: SUCCESS",
+		proj, []string{"-l", "."},
+	)
+
+	pipeline[3] = newTimeoutStep(
+		"git push",
+		"git",
+		"Git Push: SUCCESS",
+		proj, []string{"push", "origin", "main"},
+		10*time.Second,
+	)
 
 	for _, s := range pipeline {
 		msg, err := s.execute()
