aa606b6d75af1743bf3370af51e9e2c07213ce1a
adamwoolhether <adamwoolhether@gmail.com>
2022-02-14 21:35:47 +0800

    Added pScan structur and scan package for host list management with tests

---
+++ 07_cobra_cli/pScan/cmd/root.go
@@ -0,0 +1,56 @@
+/*
+Copyright © 2022 Adam Woolhether
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"os"
+	
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:     "pScan",
+	Version: "0.1",
+	Short:   "Fast TCP port scanner",
+	Long: `pScan - short for Port Scanner - executes TCP port scan
+on a list of hosts.
+
+pScan allows you to add, list, and delete hosts from the list.
+
+pScan executes a port scan on specified TCP ports. You can customize the
+target ports using a command line flag.`,
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// cobra.OnInitialize(initConfig)
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pScan.yaml)")
+	
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	
+	versionTemplate := `{{printf "%s: %s - version %s\n" .Name .Short .Version}}`
+	rootCmd.SetVersionTemplate(versionTemplate)
+}
