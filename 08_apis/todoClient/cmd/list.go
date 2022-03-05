7170d205cb554b218810c14927dc72c2fb4d89ce
adamwoolhether <adamwoolhether@gmail.com>
2022-03-05 19:44:48 +0800

    added active flag to only show incomplete tasks

--- 08_apis/todoClient/cmd/list.go
+++ 08_apis/todoClient/cmd/list.go
@@ -31,28 +31,37 @@ var listCmd = &cobra.Command{
 	SilenceUsage: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		apiRoot := viper.GetString("api-root")
+		active, err := cmd.Flags().GetBool("active")
+		if err != nil {
+			return err
+		}
 		
-		return listAction(os.Stdout, apiRoot)
+		return listAction(os.Stdout, apiRoot, active)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+	listCmd.Flags().BoolP("active", "a", false, "Display only incomplete tasks")
 }
 
-func listAction(out io.Writer, apiRoot string) error {
+func listAction(out io.Writer, apiRoot string, active bool) error {
 	items, err := getAll(apiRoot)
 	if err != nil {
 		return err
 	}
 	
-	return printAll(out, items)
+	return printAll(out, items, active)
 }
 
-func printAll(out io.Writer, items []item) error {
+func printAll(out io.Writer, items []item, active bool) error {
 	w := tabwriter.NewWriter(out, 3, 2, 0, ' ', 0)
 	
 	for k, v := range items {
+		if v.Done && active {
+			continue
+		}
+		
 		done := "-"
 		if v.Done {
 			done = "X"
