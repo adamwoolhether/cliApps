ac06c5c0a9b72d26421fd0513c08e6504fce33de
adamwoolhether <adamwoolhether@gmail.com>
2022-03-14 15:10:17 +0800

    setup interval tests, fixed bugs

---
+++ 09_interactive_tools/pomo/pomodoro/inMemory_test.go
@@ -0,0 +1,15 @@
+package pomodoro_test
+
+import (
+	"testing"
+	
+	"github.com/adamwoolhether/cliApps/09_interactive_tools/pomo/pomodoro"
+	"github.com/adamwoolhether/cliApps/09_interactive_tools/pomo/pomodoro/repository"
+)
+
+// getRepo returns the repo instance and a cleanup func.
+func getRepo(t *testing.T) (pomodoro.Repository, func()) {
+	t.Helper()
+	
+	return repository.NewInMemoryRepo(), func() {}
+}
